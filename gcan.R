62f772f2f7dfa53ce7716b966540d0e1aabe53f6
Christian Diener <ch.diener@gmail.com>
2014-12-09 18:33:45 -0600

    Put in library and enabled better name detection

--- gcan.R
+++ gcan.R
@@ -13,60 +13,11 @@ OD.max = 1.1
 args = args[1]
 
 # Functions
-read.gc = function(name)
-{
-    ftype = tolower( strsplit(args, '\\.')[[1]][2] )
-    print(ftype)
-    if( ftype=="csv" ) data = read.csv(name, header=T)
-    else if( ftype=="txt" ) data = read.table(name, header=1)
-    else stop("Not a recognized file type. Must be txt or csv!")
 
-	return(data)
-}
-
-normalize = function(data)
-{
-	ODs = as.matrix(data[,-1])
-	ODs = apply(ODs, 1, function(row) row-(ODs[1,]-OD.ref)) 
-	data[,-1] = t(ODs)
-	return(data)
-}
-
-lm_stats = function(OD, Time)
-{
-	# Calculate the area under the curve
-	fun = approxfun(Time, OD, method="linear")
-	ABC = integrate(fun, min(Time), max(Time))$value
-	
-	good = which(OD>OD.min & OD<OD.max)
-	if(length(good)>4)
-	{
-		logOD = log(OD[good])
-		Time = Time[good]
-		model = lm(logOD ~ Time)
-		stats = c( coef(model), anova(model)[1,5], length(good), ABC)
-	}
-	else stats = c(0, 0, 1, length(good), ABC)
-	
-	return(stats)
-}
-
-lm.stats.multi = function(data)
-{
-	Time = data$Time
-	probes = names(data[,-1])
-	stats = t( apply(as.matrix(data[,-1]), 2, function(od) lm_stats(od, Time)) )
-	stats = as.data.frame(stats)
-	names(stats) = c("Intercept", "Growth.rate", "pval", "N_reg", "area")
-	stats$Probe = sub("\\..+","", probes)
-
-	return(stats)
-}
-
+source('libgc.R')
 
 cat("Reading growth curves...\n")
 gcs = read.gc(args)
-OD.ref = gcs[1,2]
 
 #Normalize the data
 cat("Normalizing...\n")
@@ -75,7 +26,6 @@ gcs = normalize(gcs)
 # Get all model statistics
 cat("Calculate Linear Regressions...\n")
 models = lm.stats.multi(gcs)
-rownames(models) <- NULL
 
 # Plots
 pdf("linear_parts.pdf", width=8, height=6)
@@ -90,27 +40,30 @@ dev.off()
 
 cat("Plotting...\n")
 
-growth.plot = ggplot(models, aes(x=Probe, y=Growth.rate, group=Probe)) +
-		geom_boxplot( aes(fill=Probe) ) + xlab("") + theme_bw()
-        
-area.plot = ggplot(models, aes(x=Probe, y=area, group=Probe)) +
-		geom_boxplot( aes(fill=Probe) ) + xlab("") + theme_bw()
+growth.plot = ggplot(models, aes(x=treatment, y=growth.rate)) +
+		geom_boxplot( aes(fill=treatment) ) + xlab("") + theme_bw()
+
+
+area.plot = ggplot(models, aes(x=treatment, y=area)) +
+		geom_boxplot( aes(fill=treatment) ) + xlab("") + theme_bw()
 
 # to get the original growth curves in nice formatting
 new = melt(gcs, id.vars="Time", variable.name="Probe", value.name="OD600")
-new$Probe = sub("\\..+","", new$Probe)
+fs = names_conv(new$Probe)
+new  <- cbind(new, fs)
 
-curves.plot = ggplot(new, aes(x=Time, y=OD600, col=Probe, shape=Probe)) + 
-		geom_pointrange(stat="summary", fun.data=mean_sdl, mult=1, size=1 ) +
-		geom_line(stat="summary", fun.y=mean, size=1) + theme_bw()
+curves.plot = ggplot(new, aes(x=Time, y=OD600, col=treatment)) + 
+		geom_linerange(stat="summary", fun.data=mean_sdl, mult=1, size=1, alpha=0.5 ) +
+		geom_line(stat="summary", fun.y=mean, size=1) + facet_grid(~strain) + 
+		xlab("Time [h]") + theme_bw()
 
 cat(" - curves\n")		
-ggsave(curves.plot, file="curves.pdf", width=8, height=6)
+ggsave(curves.plot, file="curves.svg", width=6, height=4)
 
 cat(" - growth rates\n")
-ggsave(growth.plot, file="growth.pdf", width=6, height=4)
+ggsave(growth.plot, file="growth.svg", width=6, height=4)
 
 cat(" - area under curves\n")
-ggsave(area.plot, file="area.pdf", width=6, height=4)
+ggsave(area.plot, file="area.svg", width=6, height=4)
 
 capture.output(print(models), file="results.txt")
---
+++ libgc.R
@@ -0,0 +1,88 @@
+#  libgc.R
+#  
+#  Copyright 2014 Christian Diener <christian@giiku.com>
+#  
+#  MIT license. See LICENSE for more information.
+
+read.gc = function(name)
+{
+    ftype = tolower( strsplit(args, '\\.')[[1]][2] )
+    print(ftype)
+    if( ftype=="csv" ) data = read.csv(name, header=T)
+    else if( ftype=="txt" ) data = read.table(name, header=1)
+    else stop("Not a recognized file type. Must be txt or csv!")
+	
+	data$Time = data$Time*24
+	return(data)
+}
+
+
+get_factors = function(name)
+{
+	name = gsub("\\..+", "", name)
+	splits = strsplit(name, "_")[[1]]
+	
+	if (length(splits)<2) splits = c("WT", "C", NA)
+	else if (length(splits)<3) splits = c( splits, NA )
+	else splits = c(splits[1], splits[2], 
+					paste(splits[3:length(splits)], collapse=' '))
+	
+	return( splits )
+}
+
+names_conv = function(name_list)
+{
+	splits = sapply(name_list, get_factors)
+	df_info = data.frame(strain=splits[1,], treatment=splits[2,], info=splits[3,])
+	
+	# Resort by appearance
+	for( i in 1:ncol(df_info) ) 
+		df_info[,i] = factor( df_info[,i], levels=unique(df_info[,i]) )
+	
+	return( df_info )
+}
+
+normalize = function(data)
+{
+	ODs = as.matrix(data[,-1])
+	OD.ref = min(ODs[1,])
+	ODs = apply(ODs, 1, function(row) row-(ODs[1,]-OD.ref)) 
+	data[,-1] = t(ODs)
+	return(data)
+}
+
+
+lm_stats = function(OD, Time)
+{
+	# Calculate the area under the curve
+	fun = approxfun(Time, OD, method="linear")
+	ABC = integrate(fun, min(Time), max(Time), rel.tol=1e-3)$value
+	
+	good = which(OD>OD.min & OD<OD.max)
+	if(length(good)>4)
+	{
+		logOD = log(OD[good])
+		Time = Time[good]
+		model = lm(logOD ~ Time)
+		stats = c( coef(model), anova(model)[1,5], length(good), ABC)
+	}
+	else stats = c(0, 0, 1, length(good), ABC)
+	
+	return(stats)
+}
+
+lm.stats.multi = function(data)
+{
+	Time = data$Time
+	if(max(Time)<3) Time = Time*24
+	probes = names(data[,-1])
+	stats = t( apply(as.matrix(data[,-1]), 2, function(od) lm_stats(od, Time)) )
+	stats = as.data.frame(stats)
+	names(stats) = c("intercept", "growth.rate", "pval", "n_reg", "area")
+	
+	info = names_conv(probes)
+	stats = cbind(stats, info)
+	rownames(stats) = NULL
+	
+	return(stats)
+}
